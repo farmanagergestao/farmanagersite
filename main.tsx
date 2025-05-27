2919fd3530a604c1ad743e5c20c85a403bbdaa98
farmanagergestao <raphael.volpatto@farmanager.com.br>
2025-05-27 09:57:38 -0300

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Farmanager Site no Ar!</h1>
+  </React.StrictMode>
+)
